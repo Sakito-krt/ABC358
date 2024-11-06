ad3db993c4ac70b580a22ef2ab2a75418ca01708
akira <urara000setaetel@gmail.com>
2024-11-06 17:36:33 +0900

    write the code

--- A/Program.cs
+++ A/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main()
     {
-        
+        string[] input = Console.ReadLine().Split();
+        if (input[0] == "AtCoder" && input[1] == "Land")
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
     }
 }
